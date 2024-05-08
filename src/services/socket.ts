9b48ccd22932e276abc82eb0d4f3d773de827a80
rafaelsoares07 <rafaelsoares1936@gmail.com>
2024-05-08 00:52:46 -0300

    fix: trocar url prod do backend no front react

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -1,6 +1,16 @@
 import { io } from 'socket.io-client';
+import dotenv from "dotenv"
 
-const URL = 'http://192.168.0.14:3001';
+dotenv.config()
+
+let URL;
+
+if(process.env.MODE === "DEV"){
+    URL = 'http://192.168.0.14:3001';
+}else{
+    URL="https://api.render.com/deploy/srv-cotevv7109ks73ajorm0?key=Ba3Z-CbrrYs"
+}
+
 
 export const socket = io(URL);
 
