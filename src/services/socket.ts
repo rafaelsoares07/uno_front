24b0d1c393a5800e8d305db6fc0e003fbd20c72c
Rafael <rafaelsgabriel9@gmail.com>
2024-05-08 08:30:23 -0300

    Update socket.ts url render backend

--- src/services/socket.ts
+++ src/services/socket.ts
@@ -7,7 +7,7 @@ const isDevelopment = import.meta.env.MODE == 'development';
 if(isDevelopment){
     URL = 'http://192.168.0.14:3001';
 }else{
-    URL="https://api.render.com/deploy/srv-cotevv7109ks73ajorm0?key=Ba3Z-CbrrYs"
+    URL="https://server-node-uno.onrender.com"
 }
 
 
